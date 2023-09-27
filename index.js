af57a9a7d0754f516c6f7a03bc9c6ffd593d1e25
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-09-27 18:26:09 +0200

    implement

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+  clonedBtn.innerText = "Я изменю тебя";
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+  document.body.appendChild(clonedBtn);
+})
+
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = `#9c4a1a`;
+  btn.style.color = "black";
+
 })
 
 // здесь можете создать EventListener для второй кнопки
